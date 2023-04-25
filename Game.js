1e9bcd0c37d513a0df9dba993a02084648cdc814
Mec33535 <osman33535@gmail.com>
2023-04-25 20:42:40 +0300

    Update Game.js

--- Game.js
+++ Game.js
@@ -1,20 +1,86 @@
 //Memory Game
 //Yapılacaklar
+//--Hangisinin yanlış olduğunun gösterimi
 //--Kaç karenin yanıp döneceğinin ve süresinin kullanıcıya sorulması
 //--Grid-item sayısının kullanıcıya bağlı olması (acemi, amatör, pro şeklinde buttonlamak 
 //en mantıklısı, teker teker grid-item eklenmesi simetriyi siker atar) 
 //----------------------------------------------------------------
-const myDiv = document.querySelector(".grid-container");
-const gridItems = myDiv.querySelectorAll("a");
+let myDiv = document.querySelector(".grid-container");
+let gridItems = myDiv.querySelectorAll("a");
 const addButton = document.querySelector('.addButton');
 const delButton = document.querySelector('.delButton');
 const resButton = document.querySelector('.resultButton');
-const resDiv = document.querySelector(".results");
-const gridItemsCount = gridItems.length;
+const ezButton = document.querySelector('.ezZorlukButton');
+const midButton = document.querySelector('.midZorlukButton');
+let resDiv = document.querySelector(".results");
+let gridItemsCount = gridItems.length;
+
 let setTimeoutInput = 1000
-let reactionTimeInput = 100
+let reactionTimeInput = 500
 let clickCountInput = 6
 let clickCount = 0
+
+let gridSizeSelect = document.getElementById('grid-size');
+
+//Default olarak 3x3 seçilir.
+window.addEventListener("DOMContentLoaded", function() {
+    gridSizeSelect.value = "3";
+    createGrid(3);
+  });
+
+//Select burada seçilir.
+gridSizeSelect.addEventListener('change', function () {
+    const gridSize = parseInt(gridSizeSelect.value, 10);
+    createGrid(gridSize);
+});
+
+function createGrid(size) {
+    // grid boyutunu ayarlama
+    myDiv.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    myDiv.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    
+    myDiv.innerHTML = "";
+    // grid öğelerini oluşturma
+    for (let i = 0; i < size * size; i++) {
+        const gridItem = document.createElement('a');
+        gridItem.textContent = i
+        gridItem.setAttribute("href", "#")
+        gridItem.classList.add('grid-item');
+        myDiv.appendChild(gridItem);
+        
+    }
+    gridItems = myDiv.querySelectorAll("a");
+    gridItemsCount = gridItems.length;
+
+
+    
+// Bizim seçtiğimiz kareler için
+gridItems.forEach(function (item) {
+    item.addEventListener("click", function () {
+        
+        if (clickCount >= clickCountInput) {
+            return
+        }
+        if (!sessionStorage.getItem("clicked")) {
+            let beenClicked = []
+            beenClicked.push(item.textContent);
+            sessionStorage.setItem('clicked', JSON.stringify(beenClicked));
+
+        } else {
+            let clicked = []
+            clicked = JSON.parse(sessionStorage.getItem('clicked'))
+
+            clicked.push(item.textContent);
+            sessionStorage.setItem('clicked', JSON.stringify(clicked));
+        }
+        this.classList.add("bg-red");
+        clickCount++
+    });
+});
+
+
+}
+// let newGridItems = myDiv.querySelectorAll("a")
 
 //Ekleme tuşu
 addButton.addEventListener("click", function (e) {
@@ -23,8 +89,10 @@ addButton.addEventListener("click", function (e) {
         addButton.disabled = true;
     }
     else {
-        // 1 eksiği kadar kare yanıyor. 
+        // 1 eksiği kadar kare yanıyor.
         markedGridItems(clickCountInput + 1);
+        console.log(gridItemsCount)
+        console.log(newGridItems)
         e.preventDefault();
     }
 })
@@ -36,6 +104,7 @@ delButton.addEventListener("click", function (e) {
 
 //Result Tuşu
 resButton.addEventListener("click", function (e) {
+    console.log(gridItems)
 
     let toFindData = JSON.parse(sessionStorage.getItem("sessionKey"))
     let toChooseData = JSON.parse(sessionStorage.getItem("clicked"))
@@ -51,7 +120,7 @@ resButton.addEventListener("click", function (e) {
 
         redColor()
         console.log("Başlatılmadı veya Seçim yapılmadı..")
-        return
+        return;
     }
     // toChoose ile toFind arraylerinin birbirlerinden farklı değerleri= diff
     const diff = toChooseData.filter(x => !toFindData.includes(x));
@@ -105,33 +174,33 @@ function getRandomArray(y) {
     return rndmArray;
 }
 
-// Bizim seçtiğimiz kareler için
-gridItems.forEach(function (item) {
-    item.addEventListener("click", function () {
+// // Bizim seçtiğimiz kareler için
+// gridItems.forEach(function (item) {
+//     item.addEventListener("click", function () {
+        
+//         if (clickCount >= clickCountInput) {
+//             return
+//         }
+//         if (!sessionStorage.getItem("clicked")) {
+//             let beenClicked = []
+//             beenClicked.push(item.textContent);
+//             sessionStorage.setItem('clicked', JSON.stringify(beenClicked));
 
-        if (clickCount >= clickCountInput) {
-            return
-        }
-        if (!sessionStorage.getItem("clicked")) {
-            let beenClicked = []
-            beenClicked.push(item.textContent);
-            sessionStorage.setItem('clicked', JSON.stringify(beenClicked));
+//         } else {
+//             let clicked = []
+//             clicked = JSON.parse(sessionStorage.getItem('clicked'))
 
-        } else {
-            let clicked = []
-            clicked = JSON.parse(sessionStorage.getItem('clicked'))
-
-            clicked.push(item.textContent);
-            sessionStorage.setItem('clicked', JSON.stringify(clicked));
-        }
-        this.classList.add("bg-red");
-        clickCount++
-    });
-});
+//             clicked.push(item.textContent);
+//             sessionStorage.setItem('clicked', JSON.stringify(clicked));
+//         }
+//         this.classList.add("bg-red");
+//         clickCount++
+//     });
+// });
 
 //addButton'a tıklanması fonksiyonu
 function markedGridItems(x) {
-
+    
     let randomArray = getRandomArray()
     sessionStorage.removeItem('sessionKey');
 
@@ -159,6 +228,7 @@ function markedGridItems(x) {
             }, reactionTimeInput);
         }
     }
+
 }
 
 //Restart func yarattık ki result'un içindeki
@@ -172,7 +242,7 @@ function restartFunc(x) {
     }
     clickCount = 0;
     sessionStorage.clear()
-    location.reload() //f5  
+    // location.reload() //f5  
 }
 
 //Seçilmemesi ya da başlatılmaması senaryosu için full kırmızı boya
@@ -193,4 +263,37 @@ function redColor() {
 //Yanma sönme süresi setTimeoutInput olarak tanımlandı
 //Start ile yanan karaler reactionTimeInput kadar sonra söner, zorluk ayarı için
 
+// //Ez level Zorluk
+// ezButton.addEventListener("click", function () {
+//     mediumLevel(3)
+//     console.log("")
+// })
 
+// //Medium Level Zorluk
+// midButton.addEventListener("click", function () {
+//     mediumLevel(8)
+//     console.log("")
+// })
+
+// function mediumLevel(e) {
+    
+//     const gridContainer = document.querySelector('.grid-container');
+
+//     // 4x4 grid oluşturma
+//     gridContainer.style.gridTemplateColumns = `repeat(${e}, 1fr)`;
+//     gridContainer.style.gridTemplateRows = `repeat(${e}, 1fr)`;
+
+//     // Boyutları ayarlama
+//     gridContainer.style.width = '400px';
+//     gridContainer.style.height = '400px';
+
+//     gridContainer.innerHTML = '';
+//     for (let i = 0; i < size * size; i++) {
+//         const gridItem = document.createElement('div');
+//         gridItem.classList.add('grid-item');
+//         gridContainer.appendChild(gridItem);
+//     }
+// }
+
+//<a href="#" class="grid-item">0</a>
+//<a class="grid-item">0</a>
